7531f363caa2ab10328bac318da3e38791e0a1cf
daniel-halpern <82973812+daniel-halpern@users.noreply.github.com>
2024-05-27 22:43:41 -0400

    Got ollama working and refactored

--- deployment.py
+++ deployment.py
@@ -3,8 +3,28 @@ import numpy as np
 from tensorflow.keras.applications.vgg16 import preprocess_input
 import streamlit as st
 from PIL import Image
+import ollama
 
-def predict(img):
+def main(picture = None):
+    model = load_model()
+    size = 128
+    # Draws what always is on screen
+    draw()
+
+    # Take picture
+    if picture is None:
+        picture = st.camera_input("Use camera to take a picture")
+        picture = st.file_uploader("Upload an image")
+
+    if picture is not None:
+        img = Image.open(picture)
+        predictions = predict(model, img, size)
+
+    if st.button("Ask why the sky is blue"):
+        response = ask_ollama()
+        st.write(response['message']['content'])
+
+def predict(model, img, size):
     img = img.resize((size, size))
     img = np.array(img)
     img = img.reshape((1, size, size, 3))
@@ -15,28 +35,30 @@ def predict(img):
     print(f'The model predicts that the image is most likely of class: {top3_classes[0]}, second most likely: {top3_classes[1]}, third most likely: {top3_classes[2]}')    
     return predictions
 
-# Load the pre-trained model
-model = tf.keras.models.load_model('my_model.keras')
-size = 128
+def ask_ollama():
+    response = ollama.chat(model='llama3', messages=[
+    {
+        'role': 'user',
+        'content': 'Why is the sky blue? Respond with 1 sentence.',
+    },
+    ])
+    print(response['message']['content'])
+    return response
 
-# Streamlit setup
-st.set_page_config(
-     page_title="Food Allergy Detector",
-     page_icon="📍",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'About': "Show where you want community improvements!"
-     }
-)
-st.markdown("## A tool to detect if a food is safe to eat")
+def draw():
+    # Streamlit setup
+    st.set_page_config(
+        page_title="Food Allergy Detector",
+        page_icon="📍",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'About': "Show where you want community improvements!"
+        }
+    )
+    st.markdown("## A tool to detect if a food is safe to eat")
 
-# Take picture
-picture = st.camera_input("Use camera to take a picture")
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return tf.keras.models.load_model('my_model.keras')
 
-# If the user does not take a photo, import 
-if picture is None:
-    picture = st.file_uploader("Upload an image")
-
-if picture is not None:
-    img = Image.open(picture)
-    predictions = predict(img)
+main()
